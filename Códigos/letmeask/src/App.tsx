36f01b16e39a054578a0b585ac23f5bdd7e2e55a
LeticiaAlmeida16 <leticia.almeidaaa2008@gmail.com>
2025-09-11 22:19:50 -0300

    AULA01

---
+++ Códigos/letmeask/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import { Button } from './components/Button';
+
+
+function App() {
+  return (
+    <div>
+      <h1>FOI FOI FOI</h1>
+      <Button/>
+      <Button/>      
+      <Button/>  
+    </div>
+  );
+}
+
+export default App;
